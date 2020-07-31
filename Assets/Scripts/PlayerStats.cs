ddd932d83f362d94ef19a55f7bbe20b8306eccb1
JeffBcyc <41636467+JeffBcyc@users.noreply.github.com>
2020-07-30 21:26:39 -0400

    some game play numeric changes.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -7,6 +7,8 @@ public class PlayerStats : CharacterStats
     private int _maxHealth;
     private int _currentHealth;
     
+    
+    
     // Start is called before the first frame update
     void Start()
     {
